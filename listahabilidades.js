259daa9428145a009f4137e82b1f112f492a7a8f
Nachor777 <125920657+Nachor777@users.noreply.github.com>
2023-02-27 10:06:28 -0300

    updatelistadehabilidades.js

--- listahabilidades.js
+++ listahabilidades.js
@@ -10,10 +10,10 @@ var listahabilidad1 = document.getElementById("listahabilidad1");
 listahabilidad1.innerHTML = `
 
 
-<p> Soy una persona muy curiosa, de esas que preguntan todo y siempre quieren saber más. No puedo evitar mi deseo de aprender cosas nuevas, ya sea sobre tecnología, negocios, arte o cualquier cosa que llame mi atención.
+<p> <strong>Soy una persona muy curiosa, de esas que preguntan todo y siempre quieren saber más. No puedo evitar mi deseo de aprender cosas nuevas, ya sea sobre tecnología, negocios, arte o cualquier cosa que llame mi atención.
 Me encanta investigar, leer, ver documentales y tomar cursos en línea.
 <br>He trabajado en diferentes campos, lo que me ha permitido aprender habilidades únicas y conocer a personas interesantes. 
 Siempre trato de aplicar lo que aprendo en situaciones prácticas, lo que me ayuda a ser más eficiente y efectivo en mi trabajo.
 Además, tengo una mente creativa y una buena capacidad de resolución de problemas. 
-Soy muy sociable y siempre disfruto compartir mis conocimientos y aprender de los demás.</p>`;
+Soy muy sociable y siempre disfruto compartir mis conocimientos y aprender de los demás.</strong></p>`;
 
